6e79462ef2041ef5bef005f9123c662d39d57607
steffigracia <steffigracia886@gmail.com>
2026-07-09 22:35:34 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,7 @@
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+print("Addition of two numbers is: ", a + b)
+print("Subtraction of two numbers is: ", a - b)
+print("Multiplication of two numbers is: ", a * b)
+print("Division of two numbers is: ", a / b)
+print("Modulus of two numbers is: ", a % b)
