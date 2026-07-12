d640e084bd4ef92eb872413f5dcfba95bf463942
steffigracia <steffigracia886@gmail.com>
2026-07-13 00:30:57 +0530

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,8 @@
+def large():
+    if a > b:
+        print(f"{a} is greater than {b}")
+    else:
+        print(f"{b} is greater than {a}")
+a = int(input("Enter A value: "))
+b = int(input("Enter B value: "))
+large()
