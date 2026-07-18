e27d21bf5ce7ac4608f2a4d012753f3fbb0d22fe
steffigracia <steffigracia886@gmail.com>
2026-07-18 23:10:54 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -11,26 +11,28 @@ def fetch_users():
         query = "select * from employees"
         cursor.execute(query)
         rows = cursor.fetchall()
-        for row in rows:
-            print(row)
+        #for row in rows:
+            #print(row)
+        return rows
     except Exception as e:
         print("Error running query",e)
-    finally:
-        connection.close(connection,cursor)
+    #finally:
+        #connection.close(connection,cursor)
 
 def emp_by_id(id):
     connection = get_connection()
     if connection is None:
         print("Failed to connect to database")
         return
-    cursor = connection.cursor(dictionary = True)
+    cursor = connection.cursor()
     cursor.execute("select * from employees where employee_id = %s",(id,))
     user = cursor.fetchone()
-    print(user)
-    close_connection(connection,cursor)
+    #print(user)
+    return user
+    #close_connection(connection,cursor)
 
 def emp(id,salary):
-    connection = get_connection()
+    connection = get_connection(dictionary = True)
     if connection is None:
         print("Failed to connect Database")
         return
@@ -54,25 +56,26 @@ def multiplestudents():
     user = cursor.fetchall()
     print(user)
     close_connection(connection,cursor)
-def insertdata():
+def insertdata(data):
     connection = get_connection()
     if connection is None:
         print("Failed to connect Database")
         return
     cursor = connection.cursor()
     sql = "insert into employees values (%s, %s, %s ,%s ,%s,%s)"
-    values = (6,"Steffi","Gracia",40000,98766688,"abc45@gmail.com")
+    #values = (6,"Steffi","Gracia",40000,98766688,"abc45@gmail.com")
+    values = (data["employee_id"],data["first_name"],data["last_name"],data["salary"],data["mobile_no"],data["email"])
     cursor.execute(sql,values)
     connection.commit()
     print("Data Inserted Successfully")
-    close_connection(connection,cursor)
+    #close_connection(connection,cursor)
 
 
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     #emp_by_id(2)
     #multiplestudents()
-    insertdata()
+    #insertdata()
 
 
 
